df8feab4fbb9eec146033d4a99867269dce9c5af
Him5696 <himanshi1252@gmail.com>
2022-07-04 16:15:15 +0530

    String max Value

--- src/com/blz/generic/MaximumProblem.java
+++ src/com/blz/generic/MaximumProblem.java
@@ -3,11 +3,25 @@ package com.blz.generic;
 public class MaximumProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to generic Program");
-        Integer a = 3, b = 6, c =7;
+        Integer a = 3, b = 6, c = 7;
         Float x = 2.3f, y = 7.8f, z = 6.7f;
-        testMaximum (a, b, c);
+        String l = "Apple", m = "Banana", n = "Peach";
+        testMaximum(a, b, c);
         getMaximum(x, y, z);
+        getMaximum(l, m, n);
     }
+
+    private static void getMaximum(String l, String m, String n) {
+        String max = l;
+        if (m.compareTo(l) > 0) {
+            max = m;
+        }
+        if (n.compareTo(max) > 0) {
+            max = n;
+        }
+        System.out.println("Maximum Number of String is::" + max);
+    }
+
     private static void getMaximum(Float x, Float y, Float z) {
         Float max = x;
         if (y.compareTo(x) > 0) {
@@ -21,10 +35,10 @@ public class MaximumProblem {
 
     private static void testMaximum(Integer a, Integer b, Integer c) {
         Integer max = a;
-        if(b.compareTo(a)>0){
+        if (b.compareTo(a) > 0) {
             max = b;
         }
-        if(c.compareTo(max)>0){
+        if (c.compareTo(max) > 0) {
             max = c;
         }
         System.out.println("Maximum Number is: " + max);
