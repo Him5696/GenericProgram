f489924dcbd8652df0aad5a87523434b0295cd60
Him5696 <himanshi1252@gmail.com>
2022-07-04 16:50:12 +0530

    Refactoring to Generic Class of Generic Type

--- src/com/blz/generic/MaximumProblem.java
+++ src/com/blz/generic/MaximumProblem.java
@@ -1,22 +1,36 @@
 package com.blz.generic;
 
-public class MaximumProblem {
+public class MaximumProblem<T extends Comparable> {
+    //Instance Variable
+    T x, y, z;
+
+    //constructor
+    public MaximumProblem(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to generic Program");
-        MaximumProblem maximumVal = new MaximumProblem();
-        maximumVal.maximum(3, 6, 7);
-        maximumVal.maximum(2.3f, 7.8f, 6.7f);
-        maximumVal.maximum("Apple", "Peach", "Banana");
+
+        new MaximumProblem<Integer>(3, 6, 7).print();
+        new MaximumProblem<Float>(2.3f, 7.8f, 6.7f).print();
+        new MaximumProblem<String>("Apple", "Peach", "Banana").print();
     }
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
         T max = x;
-        if(y.compareTo(max) > 0)
+        if (y.compareTo(max) > 0)
             max = y;
-        if(z.compareTo(max) > 0)
+        if (z.compareTo(max) > 0)
             max = z;
 
         System.out.println("Maximum Number : " + max);
         return max;
+    }
+
+    public void print() {
+        maximum(x, y, z);
     }
 }
