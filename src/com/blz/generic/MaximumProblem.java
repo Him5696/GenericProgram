d27b10134caaf40ea249b6a87f1d66d25562edc1
Him5696 <himanshi1252@gmail.com>
2022-07-04 16:57:24 +0530

    Adding More than 3 Parameter

--- src/com/blz/generic/MaximumProblem.java
+++ src/com/blz/generic/MaximumProblem.java
@@ -2,35 +2,41 @@ package com.blz.generic;
 
 public class MaximumProblem<T extends Comparable> {
     //Instance Variable
-    T x, y, z;
+    T x, y, z, l, m;
 
     //constructor
-    public MaximumProblem(T x, T y, T z) {
+    public MaximumProblem(T x, T y, T z, T l, T m) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.l = l;
+        this.m = m;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to generic Program");
 
-        new MaximumProblem<Integer>(3, 6, 7).print();
-        new MaximumProblem<Float>(2.3f, 7.8f, 6.7f).print();
-        new MaximumProblem<String>("Apple", "Peach", "Banana").print();
+        new MaximumProblem<Integer>(3, 6, 7, 9, 2).print();
+        new MaximumProblem<Float>(2.3f, 7.8f, 6.7f, 5.5f, 8.9f).print();
+        new MaximumProblem<String>("Apple", "Peach", "Banana", "Mango", "Kiwi").print();
     }
 
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z, T l, T m) {
         T max = x;
         if (y.compareTo(max) > 0)
             max = y;
         if (z.compareTo(max) > 0)
             max = z;
+        if (l.compareTo(max) > 0)
+            max = l;
+        if (m.compareTo(max) > 0)
+            max = m;
 
         System.out.println("Maximum Number : " + max);
         return max;
     }
 
     public void print() {
-        maximum(x, y, z);
+        maximum(x, y, z, l, m);
     }
 }
