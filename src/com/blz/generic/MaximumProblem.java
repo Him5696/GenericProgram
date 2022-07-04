48a790993d7f07ed6b29df5e6f648087c5f61ef0
Him5696 <himanshi1252@gmail.com>
2022-07-04 16:02:59 +0530

    Integer max Value

--- src/com/blz/generic/MaximumProblem.java
+++ src/com/blz/generic/MaximumProblem.java
@@ -3,5 +3,18 @@ package com.blz.generic;
 public class MaximumProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to generic Program");
+        Integer a = 3, b = 8, c =7;
+        testMaximum (a, b, c);
+    }
+
+    private static void testMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("Maximum Number is: " + max);
     }
 }
