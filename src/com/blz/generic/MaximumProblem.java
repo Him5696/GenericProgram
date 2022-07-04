0d9e5f5f0aef0fcd56bb510f1e4413945df09602
Him5696 <himanshi1252@gmail.com>
2022-07-04 16:36:32 +0530

    Refactoring Method

--- src/com/blz/generic/MaximumProblem.java
+++ src/com/blz/generic/MaximumProblem.java
@@ -1,46 +1,22 @@
 package com.blz.generic;
 
 public class MaximumProblem {
+
     public static void main(String[] args) {
         System.out.println("Welcome to generic Program");
-        Integer a = 3, b = 6, c = 7;
-        Float x = 2.3f, y = 7.8f, z = 6.7f;
-        String l = "Apple", m = "Banana", n = "Peach";
-        testMaximum(a, b, c);
-        getMaximum(x, y, z);
-        getMaximum(l, m, n);
+        MaximumProblem maximumVal = new MaximumProblem();
+        maximumVal.maximum(3, 6, 7);
+        maximumVal.maximum(2.3f, 7.8f, 6.7f);
+        maximumVal.maximum("Apple", "Peach", "Banana");
     }
-
-    private static void getMaximum(String l, String m, String n) {
-        String max = l;
-        if (m.compareTo(l) > 0) {
-            max = m;
-        }
-        if (n.compareTo(max) > 0) {
-            max = n;
-        }
-        System.out.println("Maximum Number of String is::" + max);
-    }
-
-    private static void getMaximum(Float x, Float y, Float z) {
-        Float max = x;
-        if (y.compareTo(x) > 0) {
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0)
             max = y;
-        }
-        if (z.compareTo(max) > 0) {
+        if(z.compareTo(max) > 0)
             max = z;
-        }
-        System.out.println("Maximum Number of Float Value is: " + max);
-    }
 
-    private static void testMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(a) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        System.out.println("Maximum Number is: " + max);
+        System.out.println("Maximum Number : " + max);
+        return max;
     }
 }
